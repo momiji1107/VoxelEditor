0df007eaa6cecc87d366dc32c1ee883d2c7b35c9
momiji1107 <yokohirahazuki0607@gmail.com>
2026-08-24 20:47:24 +0900

    Add Scene View raycast
    カーソルにcolliderが当たった時の座標を表示する

--- Assets/VoxelEditor/Editor/VoxelEditorTool.cs
+++ Assets/VoxelEditor/Editor/VoxelEditorTool.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 [EditorTool("Voxel Editor")]
 public class VoxelEditorTool : EditorTool
 {
+    private Vector3 _hitPosition;
+    private bool _hasHit;
+
     public override GUIContent toolbarIcon
     {
         get
@@ -15,11 +18,64 @@ public class VoxelEditorTool : EditorTool
 
     public override void OnToolGUI(EditorWindow window)
     {
+        Event currentEvent = Event.current;
+
+        // Scene View上のマウス位置からRayを作成
+        Ray ray = HandleUtility.GUIPointToWorldRay(currentEvent.mousePosition);
+
+        // Scene上のColliderにRayを飛ばす
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            _hitPosition = hit.point;
+            _hasHit = true;
+        }
+        else
+        {
+            _hasHit = false;
+        }
+
+        // 左クリックされたか
+        if (currentEvent.type == EventType.MouseDown &&
+            currentEvent.button == 0 &&
+            !currentEvent.alt)
+        {
+            if (_hasHit)
+            {
+                Debug.Log($"Hit Position: {_hitPosition}");
+            }
+
+            currentEvent.Use();
+        }
+
+        DrawGUI();
+
+        // Scene Viewを再描画
+        if (currentEvent.type == EventType.MouseMove)
+        {
+            SceneView.RepaintAll();
+        }
+    }
+
+    private void DrawGUI()
+    {
         Handles.BeginGUI();
 
-        GUILayout.BeginArea(new Rect(10, 10, 180, 50), "Voxel Editor", GUI.skin.window);
+        GUILayout.BeginArea(
+            new Rect(10, 10, 220, 80),
+            "Voxel Editor",
+            GUI.skin.window
+        );
 
-        GUILayout.Label("Voxel Editor Tool");
+        if (_hasHit)
+        {
+            GUILayout.Label($"X : {_hitPosition.x:F2}");
+            GUILayout.Label($"Y : {_hitPosition.y:F2}");
+            GUILayout.Label($"Z : {_hitPosition.z:F2}");
+        }
+        else
+        {
+            GUILayout.Label("No Hit");
+        }
 
         GUILayout.EndArea();
 
