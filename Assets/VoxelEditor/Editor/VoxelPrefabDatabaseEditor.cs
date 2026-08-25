2e2bec809cd86fd07dac2a7d759a1ff18655cef4
momiji1107 <yokohirahazuki0607@gmail.com>
2026-08-25 12:38:10 +0900

    プロジェクト構成をUnity package向けに整理。READMEの追加。

--- Assets/VoxelEditor/Editor/VoxelPrefabDatabaseEditor.cs
+++ Assets/VoxelEditor/Editor/VoxelPrefabDatabaseEditor.cs
@@ -1,101 +1,105 @@
 using UnityEditor;
 using UnityEngine;
+using VoxelEditor.Runtime;
 
-[CustomEditor(typeof(VoxelPrefabDatabase))]
-public class VoxelPrefabDatabaseEditor
-    : Editor
+namespace VoxelEditor.Editor
 {
-    private GameObject _prefabToAdd;
-
-    public override void OnInspectorGUI()
+    [CustomEditor(typeof(VoxelPrefabDatabase))]
+    public class VoxelPrefabDatabaseEditor
+        : UnityEditor.Editor
     {
-        VoxelPrefabDatabase database = (VoxelPrefabDatabase)target;
+        private GameObject _prefabToAdd;
 
-        EditorGUILayout.LabelField(
-            "Voxel Prefabs",
-            EditorStyles.boldLabel
-        );
+        public override void OnInspectorGUI()
+        {
+            VoxelPrefabDatabase database = (VoxelPrefabDatabase)target;
 
-        EditorGUILayout.Space(5);
-
-        _prefabToAdd =
-            (GameObject)EditorGUILayout.ObjectField(
-                "Prefab",
-                _prefabToAdd,
-                typeof(GameObject),
-                false
+            EditorGUILayout.LabelField(
+                "Voxel Prefabs",
+                EditorStyles.boldLabel
             );
 
-        if (GUILayout.Button("Add Prefab"))
-        {
-            AddPrefab(database);
-        }
+            EditorGUILayout.Space(5);
 
-        EditorGUILayout.Space(10);
+            _prefabToAdd =
+                (GameObject)EditorGUILayout.ObjectField(
+                    "Prefab",
+                    _prefabToAdd,
+                    typeof(GameObject),
+                    false
+                );
 
-        EditorGUILayout.LabelField(
-            VoxelEditorTool.Localize($"Number of registered Prefabs"," 登録Prefab数") + " : {database.Prefabs.Count}"
-        );
-
-        EditorGUILayout.Space(5);
-
-        for (int i = 0; i < database.Prefabs.Count; i++)
-        {
-            GameObject prefab = database.Prefabs[i];
-
-            EditorGUILayout.BeginHorizontal();
-
-            EditorGUILayout.ObjectField(
-                prefab,
-                typeof(GameObject),
-                false
-            );
-
-            if (GUILayout.Button(
-                    "Remove",
-                    GUILayout.Width(70)))
+            if (GUILayout.Button("Add Prefab"))
             {
-                RemovePrefab(database, prefab);
-                break;
+                AddPrefab(database);
             }
 
-            EditorGUILayout.EndHorizontal();
-        }
-    }
+            EditorGUILayout.Space(10);
 
-    private void AddPrefab(VoxelPrefabDatabase database)
-    {
-        if (_prefabToAdd == null)
+            EditorGUILayout.LabelField(
+                VoxelEditorTool.Localize($"Number of registered Prefabs", " 登録Prefab数") + " : {database.Prefabs.Count}"
+            );
+
+            EditorGUILayout.Space(5);
+
+            for (int i = 0; i < database.Prefabs.Count; i++)
+            {
+                GameObject prefab = database.Prefabs[i];
+
+                EditorGUILayout.BeginHorizontal();
+
+                EditorGUILayout.ObjectField(
+                    prefab,
+                    typeof(GameObject),
+                    false
+                );
+
+                if (GUILayout.Button(
+                        "Remove",
+                        GUILayout.Width(70)))
+                {
+                    RemovePrefab(database, prefab);
+                    break;
+                }
+
+                EditorGUILayout.EndHorizontal();
+            }
+        }
+
+        private void AddPrefab(VoxelPrefabDatabase database)
         {
-            return;
+            if (_prefabToAdd == null)
+            {
+                return;
+            }
+
+            if (database.Contains(_prefabToAdd))
+            {
+                return;
+            }
+
+            Undo.RecordObject(
+                database,
+                "Add Voxel Prefab"
+            );
+
+            database.AddPrefab(_prefabToAdd);
+
+            EditorUtility.SetDirty(database);
+
+            _prefabToAdd = null;
         }
 
-        if (database.Contains(_prefabToAdd))
+        private void RemovePrefab(VoxelPrefabDatabase database, GameObject prefab)
         {
-            return;
+            Undo.RecordObject(
+                database,
+                "Remove Voxel Prefab"
+            );
+
+            database.RemovePrefab(prefab);
+
+            EditorUtility.SetDirty(database);
         }
-
-        Undo.RecordObject(
-            database,
-            "Add Voxel Prefab"
-        );
-
-        database.AddPrefab(_prefabToAdd);
-
-        EditorUtility.SetDirty(database);
-
-        _prefabToAdd = null;
-    }
-
-    private void RemovePrefab(VoxelPrefabDatabase database, GameObject prefab)
-    {
-        Undo.RecordObject(
-            database,
-            "Remove Voxel Prefab"
-        );
-
-        database.RemovePrefab(prefab);
-
-        EditorUtility.SetDirty(database);
     }
 }
--- Assets/VoxelEditor/Runtime/VoxelWorld.cs
+++ Assets/VoxelEditor/Runtime/VoxelWorld.cs
@@ -1,75 +1,76 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class VoxelWorld : MonoBehaviour
+namespace VoxelEditor.Runtime
 {
-    [Header("Grid Settings")]
-    [SerializeField]
-    private int _minimumHeight = 0;
-
-    [SerializeField]
-    private List<VoxelBlockData> _blocks = new();
-
-    public int MinimumHeight => _minimumHeight;
-
-    public IReadOnlyList<VoxelBlockData> Blocks => _blocks;
-
-    public bool IsBelowMinimumHeight(Vector3Int gridPosition)
+    public class VoxelWorld : MonoBehaviour
     {
-        return gridPosition.y < _minimumHeight;
-    }
+        [Header("Grid Settings")] [SerializeField]
+        private int _minimumHeight = 0;
 
-    public void AddBlock(
-        Vector3Int gridPosition,
-        GameObject prefab,
-        Quaternion rotation)
-    {
-        VoxelBlockData blockData =
-            new VoxelBlockData(
+        [SerializeField] private List<VoxelBlockData> _blocks = new();
+
+        public int MinimumHeight => _minimumHeight;
+
+        public IReadOnlyList<VoxelBlockData> Blocks => _blocks;
+
+        public bool IsBelowMinimumHeight(Vector3Int gridPosition)
+        {
+            return gridPosition.y < _minimumHeight;
+        }
+
+        public void AddBlock(
+            Vector3Int gridPosition,
+            GameObject prefab,
+            Quaternion rotation)
+        {
+            VoxelBlockData blockData =
+                new VoxelBlockData(
+                    gridPosition,
+                    prefab,
+                    rotation
+                );
+
+            _blocks.Add(blockData);
+        }
+
+        public void RemoveBlock(Vector3Int gridPosition)
+        {
+            for (int i = _blocks.Count - 1; i >= 0; i--)
+            {
+                if (_blocks[i].GridPosition == gridPosition)
+                {
+                    _blocks.RemoveAt(i);
+                    return;
+                }
+            }
+        }
+
+        public bool TryGetBlock(
+            Vector3Int gridPosition,
+            out VoxelBlockData blockData)
+        {
+            foreach (VoxelBlockData block in _blocks)
+            {
+                if (block.GridPosition == gridPosition)
+                {
+                    blockData = block;
+
+                    return true;
+                }
+            }
+
+            blockData = null;
+
+            return false;
+        }
+
+        public bool HasBlock(Vector3Int gridPosition)
+        {
+            return TryGetBlock(
                 gridPosition,
-                prefab,
-                rotation
+                out _
             );
-
-        _blocks.Add(blockData);
-    }
-
-    public void RemoveBlock(Vector3Int gridPosition)
-    {
-        for (int i = _blocks.Count - 1; i >= 0; i--)
-        {
-            if (_blocks[i].GridPosition == gridPosition)
-            {
-                _blocks.RemoveAt(i);
-                return;
-            }
         }
-    }
-
-    public bool TryGetBlock(
-        Vector3Int gridPosition,
-        out VoxelBlockData blockData)
-    {
-        foreach (VoxelBlockData block in _blocks)
-        {
-            if (block.GridPosition == gridPosition)
-            {
-                blockData = block;
-
-                return true;
-            }
-        }
-
-        blockData = null;
-
-        return false;
-    }
-    
-    public bool HasBlock(Vector3Int gridPosition)
-    {
-        return TryGetBlock(
-            gridPosition,
-            out _
-        );
     }
 }
